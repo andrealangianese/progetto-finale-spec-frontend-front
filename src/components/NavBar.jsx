8a453bfb59ed270fdd7bf63371feaaa5a97d4c86
andrealangianese <andre.langia.01@gmail.com>
2026-05-18 15:45:01 +0200

    fix: stile navbar completato

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom"
 import FavouritePage from "./FavouritePage"
+import '../App.css'
 
 export default function NavBar() {
     return (
         <>
-            <Link to='/'>Home</Link>
-            <Link to='/viaggi'>Viaggi</Link>
-            <Link to='/destinazioni-preferite'>Destinazioni Preferite</Link>
-            <Link to='/comparazione-viaggi'>Comparzione Viaggi</Link>
+            <nav className="navbar">
+                <div className="navbar-logo">
+                    <Link to="/" className="navbar-logo-link">
+                        <img
+                            src="/icona-viaggi.webp"
+                            alt="Logo Viaggi"
+                            className="navbar-icon"
+                        />
+                        <span className="navbar-link">Langiaroad</span>
+                    </Link>
+                </div>
+                <div className="navbar-links">
+                    <Link to='/' className="navbar-link">Home</Link>
+                    <Link to='/viaggi' className="navbar-link">Viaggi</Link>
+                    <Link to='/destinazioni-preferite' className="navbar-link">Destinazioni Preferite</Link>
+                    <Link to='/comparazione-viaggi' className="navbar-link">Comparazione Viaggi</Link>
+                </div>
+            </nav>
         </>
     )
 }
