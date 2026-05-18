6248a3837f3dbf57be3da2712fe25885aaef61d1
andrealangianese <andre.langia.01@gmail.com>
2026-05-18 17:01:41 +0200

    feat: barra di ricerca per filtrare per titolo attività

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function HomePage() {
 
     // state per salvare i viaggi
     const [viaggi, setViaggi] = useState([])
+    // state per ricerca viaggi per nome
+    const [cercaViaggi, setCercaViaggi] = useState('')
 
     useEffect(() => {
         fetch('http://localhost:3001/travels')
@@ -11,12 +13,23 @@ export default function HomePage() {
             .then(data => setViaggi(data))
             .catch(error => console.error(error)
             )
+    }, [])
+
+    const viaggiFiltrati = viaggi.filter(viaggio => {
+        const titolo = viaggio.title.toLowerCase()
+        const comparazioneTitolo = titolo.includes(cercaViaggi.toLowerCase())
+        return comparazioneTitolo
     })
     return (
         <>
             <h1>I nostri viaggi</h1>
-            {viaggi.length > 0 ? (
-                viaggi.map(viaggio => (
+            <input
+                type="text"
+                placeholder="Cerca per titolo"
+                onChange={e => setCercaViaggi(e.target.value)}
+            />
+            {viaggiFiltrati.length > 0 ? (
+                viaggiFiltrati.map(viaggio => (
                     <div key={viaggio.id}>
                         <h3>{viaggio.title}</h3>
                         <p>Categoria: {viaggio.category}</p>
