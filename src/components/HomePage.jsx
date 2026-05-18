f993dfa03f2498999522636e9bf72603aa32f823
andrealangianese <andre.langia.01@gmail.com>
2026-05-18 16:28:17 +0200

    feat: stampo nella homage tutti i viaggi presenti

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react"
+
 export default function HomePage() {
+
+    // state per salvare i viaggi
+    const [viaggi, setViaggi] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/travels')
+            .then(res => res.json())
+            .then(data => setViaggi(data))
+            .catch(error => console.error(error)
+            )
+    })
     return (
         <>
-            <h2>sarò l'homepage</h2>
+            <h1>I nostri viaggi</h1>
+            {viaggi.length > 0 ? (
+                viaggi.map(viaggio => (
+                    <div key={viaggio.id}>
+                        <h3>{viaggio.title}</h3>
+                        <p>Categoria: {viaggio.category}</p>
+                    </div>
+                ))
+            ) : <p>Non è stato trovato nulla</p>}
         </>
     )
 }
