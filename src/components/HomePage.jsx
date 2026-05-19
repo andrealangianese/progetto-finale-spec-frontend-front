3a777c89de0cf666858846295d66f2336952d15a
andrealangianese <andre.langia.01@gmail.com>
2026-05-19 09:32:11 +0200

    feat: comparazione per categorie attraverso <option/>

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,7 +5,9 @@ export default function HomePage() {
     // state per salvare i viaggi
     const [viaggi, setViaggi] = useState([])
     // state per ricerca viaggi per nome
-    const [cercaViaggi, setCercaViaggi] = useState('')
+    const [cercaViaggiNome, setCercaViaggiNome] = useState('')
+    // state per ricerca viaggi per categorie
+    const [cercaViaggiCategoria, setCercaViaggiCategoria] = useState('Tutte le categorie')
 
     useEffect(() => {
         fetch('http://localhost:3001/travels')
@@ -15,19 +17,39 @@ export default function HomePage() {
             )
     }, [])
 
+    // per evitare di stampare tutte le categorie uguali
+    const categorieUniche = ['Tutte le categorie', ...new Set(viaggi.map(viaggio => viaggio.category).filter(Boolean))]
+
     const viaggiFiltrati = viaggi.filter(viaggio => {
         const titolo = viaggio.title.toLowerCase()
-        const comparazioneTitolo = titolo.includes(cercaViaggi.toLowerCase())
-        return comparazioneTitolo
+        const comparazioneTitolo = titolo.includes(cercaViaggiNome.toLowerCase())
+        const comprazioneCategorie = cercaViaggiCategoria === 'Tutte le categorie' || viaggio.category === cercaViaggiCategoria
+        return comparazioneTitolo && comprazioneCategorie
     })
     return (
         <>
             <h1>I nostri viaggi</h1>
+            {/* ricerca per testo */}
             <input
                 type="text"
                 placeholder="Cerca per titolo"
-                onChange={e => setCercaViaggi(e.target.value)}
+                onChange={e => setCercaViaggiNome(e.target.value)}
             />
+            {/* ricerca per categorie con select */}
+            <div>
+                <label>Filtra per categoria</label>
+                <select
+                    value={cercaViaggiCategoria}
+                    onChange={e => setCercaViaggiCategoria(e.target.value)}
+                >
+                    {/* stampo tutte le categorie solo una volta */}
+                    {categorieUniche.map((cat, index) => (
+                        <option key={index} value={cat}>
+                            {cat}
+                        </option>
+                    ))}
+                </select>
+            </div>
             {viaggiFiltrati.length > 0 ? (
                 viaggiFiltrati.map(viaggio => (
                     <div key={viaggio.id}>
